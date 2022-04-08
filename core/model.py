3061a79946430422386d6c6fa35a51a76b8d93e2
Rodrigo Vieira <rodrigo.vieira@gmail.com>
2022-04-08 14:52:03 -0300

    add urgency and importance properties with validation

--- core/model.py
+++ core/model.py
@@ -3,13 +3,22 @@ from dataclasses import dataclass
 from datetime import datetime
 
 
-@dataclass
 class Task:
-    _id = str(uuid.uuid4())
-    user_id: str
-    name: str
-    description: str
-    _created_at = datetime.now()
+    def __init__(self, user_id, name, description):
+        self.user_id = user_id
+        self.name = name
+        self.description = description
+        self._id = str(uuid.uuid4())
+        self._created_at = datetime.now()
+        self._importance = None
+        self._urgency = None
+
+    def _validate_scale_value(self, value):
+        if value < 0:
+            raise ValueError("Scale value must be positive")
+        if value > 10:
+            raise ValueError("Scale value must be at most 10")
+        return value
 
     @property
     def id(self) -> str:
@@ -18,3 +27,19 @@ class Task:
     @property
     def created_at(self) -> datetime:
         return self._created_at
+
+    @property
+    def importance(self):
+        return self._importance
+
+    @importance.setter
+    def importance(self, value):
+        self._importance = self._validate_scale_value(value)
+
+    @property
+    def urgency(self):
+        return self._urgency
+
+    @urgency.setter
+    def urgency(self, value):
+        self._urgency = self._validate_scale_value(value)
--- core/test_model.py
+++ core/test_model.py
@@ -3,12 +3,53 @@ from model import Task
 from datetime import datetime
 
 
-def test_task_is_created_with_timestamp_and_id():
-    task = Task(
+def create_valid_task():
+    return Task(
         user_id="valid_user_id",
         name="valid_name",
         description="valid_description",
     )
 
+
+def test_task_is_created_with_timestamp_and_id():
+    task = create_valid_task()
+
     assert type(task.created_at) is datetime
     assert len(task.id) == 36
+
+
+def test_task_can_have_importance_and_urgency_assigned():
+    task = create_valid_task()
+    task.importance = 5
+    task.urgency = 4
+
+    assert task.importance == 5
+    assert task.urgency == 4
+
+
+def test_throws_error_if_importance_is_less_than_minimum_allowed():
+    task = create_valid_task()
+    invalid_low_value = -1
+    with pytest.raises(ValueError):
+        task.importance = invalid_low_value
+
+
+def test_throws_error_if_importance_is_more_than_max_allowed():
+    task = create_valid_task()
+    invalid_high_value = 1000
+    with pytest.raises(ValueError):
+        task.importance = invalid_high_value
+
+
+def test_throws_error_if_urgency_is_less_than_minimum_allowed():
+    task = create_valid_task()
+    invalid_low_value = -1
+    with pytest.raises(ValueError):
+        task.urgency = invalid_low_value
+
+
+def test_throws_error_if_urgency_is_more_than_max_allowed():
+    task = create_valid_task()
+    invalid_high_value = 1000
+    with pytest.raises(ValueError):
+        task.urgency = invalid_high_value
