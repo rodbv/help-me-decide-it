60248b81112d370f51ab0640f05a9cd2d02ae5c3
Rodrigo Vieira <rodrigo.vieira@gmail.com>
2022-04-10 10:54:34 -0300

    refactor: converting task to dataclass and renaming file from model to task

--- core/task.py
+++ core/task.py
@@ -1,9 +1,10 @@
 import uuid
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from datetime import datetime
-from enum import Enum
+from enum import Enum, auto
 
 
+@dataclass
 class Task:
     """A task is one item which has a name, description and is positioned in the
     Eisenhower matrix by setting the properties urgency and importance (from 0 to 10)
@@ -15,34 +16,24 @@ class Task:
         A task
     """
 
-    max_scale_value = 10
-    min_scale_value = -10
+    MAX_SCALE_VALUE = 10
+    MIN_SCALE_VALUE = -10
 
-    def __init__(self, owner, name, description):
-        self.owner = owner
-        self.name = name
-        self.description = description
-        self._id = str(uuid.uuid4())
-        self._created_at = datetime.now()
-        self._importance = None
-        self._urgency = None
+    owner: str
+    name: str
+    description: str
+    created_at: datetime = field(init=False, default_factory=datetime.now)
+    _urgency: int = field(init=False, repr=False)
+    _importance: int = field(init=False, repr=False)
 
     def _validate_scale_value(self, value):
-        if value < self.min_scale_value or value > self.max_scale_value:
+        if value < self.MIN_SCALE_VALUE or value > self.MAX_SCALE_VALUE:
             raise ValueError(
                 "Value must be between {0} and {1}".format(
-                    self.min_scale_value, self.max_scale_value
+                    self.MIN_SCALE_VALUE, self.MAX_SCALE_VALUE
                 )
             )
         return value
-
-    @property
-    def id(self) -> str:
-        return self._id
-
-    @property
-    def created_at(self) -> datetime:
-        return self._created_at
 
     @property
     def importance(self):
@@ -83,7 +74,7 @@ class Task:
 
 
 class Quadrants(Enum):
-    do_now = 1
-    schedule = 2
-    delegate = 3
-    eliminate = 4
+    do_now = auto()
+    schedule = auto()
+    delegate = auto()
+    eliminate = auto()
--- core/test_task.py
+++ core/test_task.py
@@ -1,10 +1,10 @@
 import pytest
-from model import Task, Quadrants
+from task import Task, Quadrants
 from datetime import datetime
 
 
-valid_high = 5
-valid_low = -5
+valid_high = Task.MAX_SCALE_VALUE
+valid_low = Task.MIN_SCALE_VALUE
 
 
 def create_valid_task():
@@ -15,37 +15,35 @@ def create_valid_task():
     )
 
 
-def test_task_is_created_with_timestamp_and_id():
+def test_task_is_created_with_timestamp():
     task = create_valid_task()
-
     assert type(task.created_at) is datetime
-    assert len(task.id) == 36
 
 
 def test_throws_error_if_importance_is_less_than_minimum_allowed():
     task = create_valid_task()
-    invalid_low_value = Task.min_scale_value - 1
+    invalid_low_value = Task.MIN_SCALE_VALUE - 1
     with pytest.raises(ValueError):
         task.importance = invalid_low_value
 
 
 def test_throws_error_if_importance_is_more_than_max_allowed():
     task = create_valid_task()
-    invalid_high_value = Task.max_scale_value + 1
+    invalid_high_value = Task.MAX_SCALE_VALUE + 1
     with pytest.raises(ValueError):
         task.importance = invalid_high_value
 
 
 def test_throws_error_if_urgency_is_less_than_minimum_allowed():
     task = create_valid_task()
-    invalid_low_value = Task.min_scale_value - 1
+    invalid_low_value = Task.MIN_SCALE_VALUE - 1
     with pytest.raises(ValueError):
         task.urgency = invalid_low_value
 
 
 def test_throws_error_if_urgency_is_more_than_max_allowed():
     task = create_valid_task()
-    invalid_high_value = Task.max_scale_value + 1
+    invalid_high_value = Task.MAX_SCALE_VALUE + 1
     with pytest.raises(ValueError):
         task.urgency = invalid_high_value
 
