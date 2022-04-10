e58f9a23d1794ce4002ff630a4acc21026fa77b8
Rodrigo Vieira <rodrigo.vieira@gmail.com>
2022-04-10 12:59:29 -0300

    chore: add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name="Help me Decide it",
+    version="0.1.0",
+    description="A example Python package",
+    url="https://github.com/rodbv/help-me-decide-it",
+    author="Rodrigo Vieira",
+    author_email="rodrigo.vieira@gmail.com",
+    license="MIT",
+    packages=["pytest"],
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+    ],
+)
