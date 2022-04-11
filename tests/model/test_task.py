5184591c99ba7802cefed6e977f4b868205f632d
Rodrigo Vieira <rodrigo.vieira@gmail.com>
2022-04-11 16:32:35 -0300

    refactor: organize tests grouping by type using classes

--- tests/model/test_task.py
+++ tests/model/test_task.py
@@ -19,46 +19,45 @@ def create_valid_task(urgency=None, importance=None):
     return t
 
 
-def test_task_is_created_with_timestamp():
-    t = create_valid_task()
-    assert type(t.created_at) is datetime
+class TestTask:
+    def test_task_is_created_with_timestamp(self):
+        t = create_valid_task()
+        assert type(t.created_at) is datetime
 
 
-def test_task_with_importance_2_and_urgency_3_has_ranking_10_63():
-    t = create_valid_task(urgency=3, importance=2)
-    expected = math.sqrt(8**2 + 7**2)
-    assert expected == pytest.approx(t.ranking(), RANKING_TEST_DELTA)
+class TestRanking:
+    def test_task_with_importance_2_and_urgency_3_has_ranking_10_63(self):
+        t = create_valid_task(urgency=3, importance=2)
+        expected = math.sqrt(8**2 + 7**2)
+        assert expected == pytest.approx(t.ranking(), RANKING_TEST_DELTA)
+
+    def test_task_with_importance_minus2_and_urgency_minus3_has_ranking_17_69(self):
+        t = create_valid_task(urgency=-3, importance=-2)
+        expected = math.sqrt(13**2 + 12**2)
+        assert expected == pytest.approx(t.ranking(), RANKING_TEST_DELTA)
 
 
-def test_task_with_importance_minus2_and_urgency_minus3_has_ranking_17_69():
-    t = create_valid_task(urgency=-3, importance=-2)
-    expected = math.sqrt(13**2 + 12**2)
-    assert expected == pytest.approx(t.ranking(), RANKING_TEST_DELTA)
+class TestTaskValidation:
+    def test_throws_error_if_importance_is_less_than_minimum_allowed(self):
+        t = create_valid_task()
+        invalid_low_value = task.Task.MIN_SCALE_VALUE - 1
+        with pytest.raises(ValueError):
+            t.importance = invalid_low_value
 
+    def test_throws_error_if_importance_is_more_than_max_allowed(self):
+        t = create_valid_task()
+        invalid_high_value = task.Task.MAX_SCALE_VALUE + 1
+        with pytest.raises(ValueError):
+            t.importance = invalid_high_value
 
-def test_throws_error_if_importance_is_less_than_minimum_allowed():
-    t = create_valid_task()
-    invalid_low_value = task.Task.MIN_SCALE_VALUE - 1
-    with pytest.raises(ValueError):
-        t.importance = invalid_low_value
+    def test_throws_error_if_urgency_is_less_than_minimum_allowed(self):
+        t = create_valid_task()
+        invalid_low_value = task.Task.MIN_SCALE_VALUE - 1
+        with pytest.raises(ValueError):
+            t.urgency = invalid_low_value
 
-
-def test_throws_error_if_importance_is_more_than_max_allowed():
-    t = create_valid_task()
-    invalid_high_value = task.Task.MAX_SCALE_VALUE + 1
-    with pytest.raises(ValueError):
-        t.importance = invalid_high_value
-
-
-def test_throws_error_if_urgency_is_less_than_minimum_allowed():
-    t = create_valid_task()
-    invalid_low_value = task.Task.MIN_SCALE_VALUE - 1
-    with pytest.raises(ValueError):
-        t.urgency = invalid_low_value
-
-
-def test_throws_error_if_urgency_is_more_than_max_allowed():
-    t = create_valid_task()
-    invalid_high_value = task.Task.MAX_SCALE_VALUE + 1
-    with pytest.raises(ValueError):
-        t.urgency = invalid_high_value
+    def test_throws_error_if_urgency_is_more_than_max_allowed(self):
+        t = create_valid_task()
+        invalid_high_value = task.Task.MAX_SCALE_VALUE + 1
+        with pytest.raises(ValueError):
+            t.urgency = invalid_high_value
